6c0b2a62af39e8cdb5b6902f485288b1551947c4
Yasser <yassergeo@gmail.com>
2020-10-28 12:27:17 -0500

    added matain text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,33 @@
 import "./App.css";
-import { Col, Row } from "reactstrap";
+import { Col, Row, Container } from "reactstrap";
 import logo from "./images/logo.png";
+import { Icon } from "react-icons-kit";
+import { warning } from "react-icons-kit/icomoon/warning";
 
 function App() {
   return (
-    <Row className="Contenedor justify-content-center">
-      <Col xs={2} className="Contenedor">
-        <img src={logo} alt="log" height="100" width="210"></img>
-      </Col>
-    </Row>
+    <Container>
+      <Row className="justify-content-center">
+        <Col>
+          <Row className="justify-content-center">
+            <img src={logo} alt="log" height="100" width="210"></img>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center Icon_color Contenedor_Icono">
+        <Icon icon={warning} size={150} />
+      </Row>
+      <Container>
+        <Row className="justify-content-center Contenedor_Texto Contenedor_Icono">
+          <p>
+            Estimados clientes, estamos haciendo mejoras en nuestra
+            infraestructura, para poder seguir brindando un servicio de
+            excelencia.
+          </p>
+          <p>Les pedimos disculpas por las molestias ocasionadas.</p>
+        </Row>
+      </Container>
+    </Container>
   );
 }
 
